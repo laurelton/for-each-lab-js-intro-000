fae58cd04a64bbe5f756daedd319dfde968a46f0
Frank Rose <frankrosework@gmail.com>
2018-03-31 00:43:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var iterativeLog = function(arr) {
   arr.forEach((elem, indx) => console.log(`${indx}: ${elem}`));
+
+var iterate = function(func) {
+  var arr = [2, 3, 5, 7, 11, 13, 17, 19, 'love/hate', 'cats/dogs'];
 }
